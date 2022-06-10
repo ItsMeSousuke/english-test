f21456f590958bd16e87c9608dbe6f052cbaab26
Adam Jaguś <13621655+ItsMeSousuke@users.noreply.github.com>
2022-06-10 22:37:33 +0200

    fix review button

--- english-test/wwwroot/js/canvas.js
+++ english-test/wwwroot/js/canvas.js
@@ -1,48 +1,49 @@
-﻿//var mousePressed = false;
-//var lastX, lastY;
-//var ctx;
+﻿var mousePressed = false;
+var lastX, lastY;
+var ctx;
+const canvas = document.querySelector('canvas')
 
-//function initThis() {
-//    console.log(document.querySelector('draw'))
-//    ctx = document.getElementById('draw').getContext("2d");
+function initThis() {
+    console.log(document.querySelector('canvas'))
+    ctx = document.getElementById('draw').getContext("2d");
 
-//    $('#draw').mousedown(function (e) {
-//        mousePressed = true;
-//        Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, false);
-//    });
+    $('#draw').mousedown(function (e) {
+        mousePressed = true;
+        Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, false);
+    });
 
-//    $('#draw').mousemove(function (e) {
-//        if (mousePressed) {
-//            Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, true);
-//        }
-//    });
+    $('#draw').mousemove(function (e) {
+        if (mousePressed) {
+            Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, true);
+        }
+    });
 
-//    $('#draw').mouseup(function (e) {
-//        mousePressed = false;
-//    });
-//    $('#draw').mouseleave(function (e) {
-//        mousePressed = false;
-//    });
-//}
+    $('#draw').mouseup(function (e) {
+        mousePressed = false;
+    });
+    $('#draw').mouseleave(function (e) {
+        mousePressed = false;
+    });
+}
 
-//function Draw(x, y, isDown) {
-//    if (isDown) {
-//        ctx.beginPath();
-//        ctx.strokeStyle = 'black'// $('#selColor').val();
-//        ctx.lineWidth = 3 // $('#selWidth').val();
-//        ctx.lineJoin = "round";
-//        ctx.moveTo(lastX, lastY);
-//        ctx.lineTo(x, y);
-//        ctx.closePath();
-//        ctx.stroke();
-//    }
-//    lastX = x; lastY = y;
-//}
+function Draw(x, y, isDown) {
+    if (isDown) {
+        ctx.beginPath();
+        ctx.strokeStyle = 'black'// $('#selColor').val();
+        ctx.lineWidth = 3 // $('#selWidth').val();
+        ctx.lineJoin = "round";
+        ctx.moveTo(lastX, lastY);
+        ctx.lineTo(x, y);
+        ctx.closePath();
+        ctx.stroke();
+    }
+    lastX = x; lastY = y;
+}
 
-//function clearArea() {
-//    // Use the identity matrix while clearing the canvas
-//    ctx.setTransform(1, 0, 0, 1, 0, 0);
-//    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-//}
+function clearArea() {
+    // Use the identity matrix while clearing the canvas
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+}
 
-//initThis();
+initThis();
